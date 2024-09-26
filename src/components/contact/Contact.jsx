2adb504dccc25595922aa3ae0201d7272a73f689
ApurvaSi16 <apurva.singh2020@vitstudent.ac.in>
2024-09-26 11:41:53 +0530

    Your commit message

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./contact.css";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_gkhfv4t', 
+        'template_6pw7g72', 
+        form.current, {
+        publicKey: 'o-1jUdvD9M3tpNLJa',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert("Message sent!")
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <section className="contact container section" id='contact'>
       <h2 className="section__title">Get In Touch</h2>
@@ -12,27 +36,27 @@ const Contact = () => {
           <p className="contact__details">Don't like forms? Send me an email. 👋</p>
         </div>
 
-        <form action="" className="contact__form">
+        <form ref={form} onSubmit={sendEmail} className="contact__form">
           <div className="contact__form-group">
             <div className="contact__form-div">
               <input type="text" className="contact__form-input"
-              placeholder='Insert your name' />
+              placeholder='Insert your name' name="user_name" />
             </div>
 
             <div className="contact__form-div">
               <input type="email" className="contact__form-input"
-              placeholder='Insert your email' />
+              placeholder='Insert your email' name="user_email"/>
             </div>
           </div>
 
             <div className="contact__form-div">
               <input type="text" className="contact__form-input"
-              placeholder='Insert your subject' />
+              placeholder='Insert your subject' name="user_subject"/>
             </div>
 
             <div className="contact__form-div contact__form-area">
               <textarea 
-              name="" 
+              name="message" 
               id="" 
               cols="30" 
               rows="10"
@@ -41,7 +65,7 @@ const Contact = () => {
               ></textarea>
             </div>
 
-            <button className="btn">Send Message</button>
+            <input type='submit' className="btn" value="Send"/>
         </form>
       </div>
     </section>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -35,7 +35,7 @@ const Portfolio = () => {
 
               <span className="work__category">{category}</span>
               <h3 className="work__title">{title}</h3>
-              <a href="#" className="work__button">
+              <a href="https://github.com/ApurvaSi16" className="work__button">
                 <i className="icon-link work__button-icon"></i>
               </a>
             </div>
